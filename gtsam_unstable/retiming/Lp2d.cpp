b0c05cd45738ffaa96766ddb8d86a7b83e524850
Gerry Chen <gerry.chen2015@gmail.com>
2023-09-06 22:02:13 -0400

    early exit since convex implies local extremum is global extremum

--- gtsam_unstable/retiming/Lp2d.cpp
+++ gtsam_unstable/retiming/Lp2d.cpp
@@ -8,46 +8,44 @@ namespace lp2d {
 /******************************************************************************/
 
 ScalarBounds extremalsY(const Inequalities& inequalities) {
-  double lower_bound = std::numeric_limits<double>::lowest(),
-         upper_bound = std::numeric_limits<double>::max();
+  ScalarBounds bounds = ScalarBounds::Zero();
+
+  // Loop over every unique pair of inequalities
   for (int i = 0; i < inequalities.rows(); ++i) {
     for (int j = i + 1; j < inequalities.rows(); ++j) {
       const auto& l1 = inequalities.row(i);
       const auto& l2 = inequalities.row(j);
+
       // We only care about bottom-boundaries or top-boundaries
       // One way to check this is to check if the signs of the x-components of
       // the normal vectors are different.  Special case: if either has cx=0,
       // that's a horizontal line and we want to keep it.
       if (l1(0) * l2(0) > 0) continue;
+
       // Manually check for parallel lines to reduce catch/throw overhead
       if (abs(l1(0) * l2(1) - l1(1) * l2(0)) < 1e-12) continue;
+
+      // Compute intersection point
       auto p = intersection(l1, l2);
-      if (isFeasible(inequalities, p)) {  // Valid vertex
-        // To determine whether it's an upper or lower bound, check the
-        // direction of the "averaged" normal vector is pointing up or down.
-        // Note: due to the <= convention, normal vector points away from the
-        // feasible region
-        if ((l1.head<2>().normalized() + l2.head<2>().normalized())(1) < 0) {
-          // vector pointing down -> feasible region above -> lower bound
-          lower_bound = std::max(lower_bound, p(1));
-        } else {
-          upper_bound = std::min(upper_bound, p(1));
-        }
+      if (!isFeasible(inequalities, p)) continue;
+
+      // To determine whether it's an upper or lower bound, check the
+      // direction of the "averaged" normal vector is pointing up or down.
+      // Note: due to the <= convention, normal vector points away from the
+      // feasible region
+      // Also, because it's convex, this is the global max
+      if ((l1.head<2>().normalized() + l2.head<2>().normalized())(1) < 0) {
+        // vector pointing down -> feasible region above -> lower bound
+        bounds.row(1) << -1, -p(1);       // -y <= -p1 (lower bound)
+        if (bounds(0, 0)) return bounds;  // early exit
+      } else {
+        bounds.row(0) << 1, p(1);         // y <= p1 (upper bound)
+        if (bounds(1, 0)) return bounds;  // early exit
       }
     }
   }
 
-  bool has_lower = lower_bound > std::numeric_limits<double>::lowest(),
-       has_upper = upper_bound < std::numeric_limits<double>::max();
-  if (has_lower && has_upper) {
-    return (ScalarBounds() << 1, upper_bound, -1, -lower_bound).finished();
-  } else if (!has_lower && has_upper) {
-    return (ScalarBounds() << 1, upper_bound, 0, 0).finished();
-  } else if (has_lower && !has_upper) {
-    return (ScalarBounds() << 0, 0, -1, -lower_bound).finished();
-  } else {
-    return ScalarBounds::Zero();
-  }
+  return bounds;
 }
 
 /******************************************************************************/
